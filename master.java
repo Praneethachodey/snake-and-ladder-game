0e1061ee3a9c3520c57a56da35d0c00ff15b29a3
Praneethachodey <praneethachodey@gmail.com>
2020-09-24 01:29:47 +0530

    [Praneetha]Added: uc2 generate dice

--- master.java
+++ master.java
@@ -4,5 +4,8 @@ public static void main(String[] args)
 {
 System.out.println("welcome to snake and ladder game");
 player p1 = new player();
+int dice=0;
+dice = p1.rollDice();
+System.out.println("number on dice : " + dice);
 }
 }
--- player.java
+++ player.java
@@ -1,9 +1,16 @@
 public class player{
  int position_of_player;
+ int dice=0;
  public player()
-{
-position_of_player=0;
-System.out.println("player created with starting point: 0 ");
+  {
+   position_of_player=0;
+   System.out.println("player created with starting point: 0 ");
+  }
+ 
+ int rollDice()
+  {
+   dice =(int)Math.floor(Math.random()*6)+1;
+   return dice;
+  } 
 
-}
 }
